4b37fbc3d60dba721e8b87f202046862c3c58151
Rafie Rasydan <rafierasyw@gmail.com>
2025-05-27 17:02:58 +0700

    header manusia done

--- komposisi/manusia.h
+++ komposisi/manusia.h
@@ -5,5 +5,18 @@ using namespace std;
 class manusia 
 {
     public: 
-    
-}
+    string name;
+    jantung varJantung;
+
+    manusia(string pName)
+    : name(pName)
+    {
+        cout << name << " hidup\n";
+
+    }
+    ~manusia() 
+    {
+        cout << name << " mati\n";
+    }
+};
+#endif
