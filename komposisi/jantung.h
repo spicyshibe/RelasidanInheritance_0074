ea3dc91bc425ce561a23cc5c9b894ae484bf3c23
Rafie Rasydan <rafierasyw@gmail.com>
2025-05-27 16:58:51 +0700

    jantung done

--- komposisi/jantung.h
+++ komposisi/jantung.h
@@ -6,5 +6,14 @@ using namespace std;
 class jantung
 {
     public: 
-    
-}
+        jantung()
+        {
+            cout << "jantung dihidupkan\n";
+
+        }
+        ~jantung() 
+        {
+            cout << "jantung dimatikan\n";
+        }
+};
+#endif
